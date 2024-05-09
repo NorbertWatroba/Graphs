c131dbfa4fda9b86b198da0047f3d304d4d8f97e
NorbertWatroba <norbert@watroba.net>
2024-05-09 18:41:48 +0200

    all obligatory methods implementation

--- Graph.py
+++ Graph.py
@@ -5,21 +5,26 @@ class Graph:
     def __init__(self, number: int):
         self.nodes: dict[str, list] = dict()
         for i in range(1, number+1):
-            self.nodes[str(i)] = input(f'  {i:>3}> ').split()
+            self.nodes[str(i)] = [v for v in input(f'    {i}> ').split() if int(v) in range(1, number+1)]
 
     def print_list(self):
         for node in self.nodes.keys():
             connections = ' '.join(self.nodes[node])
-            print(f'{node:>3}> {connections}')
+            print(f'{node}> {connections}')
 
     def print_matrix(self):
         # header
         print(f'  | {" ".join(list(self.nodes.keys()))}')
-        print(f'--+{"--" * len(self.nodes.keys())}')
+        print(f'--+{"--" * len(self.nodes)}')
         # body
         for node in self.nodes.keys():
             row = ['1' if i in self.nodes[node] else '0' for i in self.nodes.keys()]
             print(f'{node} | {" ".join(row)}')
+
+    def print_table(self):
+        for node in self.nodes.keys():
+            for edge in self.nodes[node]:
+                print(f'({node},{edge})')
 
     def find_edge(self, frm: str, to: str):
         if self.nodes.get(frm) and to in self.nodes.get(frm):
@@ -50,12 +55,50 @@ class Graph:
             for neighbour in self.nodes[current]:
                 self._dfs(visited, neighbour)
 
+    def kahn_sort(self):
+        incoming_edges = {k: 0 for k in self.nodes.keys()}
+        for node in self.nodes.keys():
+            for edge in self.nodes[node]:
+                incoming_edges[edge] += 1
 
+        que = deque([k for k, v in incoming_edges.items() if v == 0])
+        order = []
 
+        while que:
+            node = que.popleft()
+            order.append(node)
+            for edge in self.nodes[node]:
+                incoming_edges[edge] -= 1
+                if incoming_edges[edge] == 0:
+                    que.append(edge)
 
+        if len(order) == len(self.nodes):
+            print(f'Topologically sorted graph: {" ".join(order)}')
+        else:
+            print('Cycles found in the graph!')
 
+    def tarjan_sort(self):
+        def dfs(node):
+            if node in perm:
+                return
+            perm.add(node)
+            temp.add(node)
+            for neighbor in self.nodes[node]:
+                if neighbor not in perm:
+                    dfs(neighbor)
+                elif neighbor in temp:
+                    raise ValueError('Cycles found in the graph!')
+            temp.remove(node)
+            sorted_nodes.append(node)
 
+        perm = set()
+        temp = set()
+        sorted_nodes = []
 
-
-
-
+        for key in self.nodes.keys():
+            try:
+                dfs(key)
+            except ValueError as e:
+                print(e)
+                return
+        print(f'Topologically sorted graph: {" ".join(sorted_nodes[::-1])}')
--- program.py
+++ program.py
@@ -9,14 +9,34 @@ while True:
             graph.print_matrix()
         case 'print list' | 'pl':
             graph.print_list()
+        case 'print table' | 'pt':
+            graph.print_table()
         case 'find edge' | 'f':
-            frm = input('from> ').strip()
-            to = input('  to> ').strip()
+            frm = input('  from> ').strip()
+            to = input('    to> ').strip()
             graph.find_edge(frm, to)
         case 'bfs':
             graph.bfs()
         case 'dfs':
             graph.dfs()
+        case 'kahn' | 'ks':
+            graph.kahn_sort()
+        case 'tarjan' | 'ts':
+            graph.tarjan_sort()
+        case 'help' | 'h':
+            print('''
++================================================================================+
+|  Help          | h   |   Show this message                                     |
+|  Print Matrix  | pm  |   Print matrix representation                           |
+|  Print List    | pl  |   Print list representation                             |
+|  Print Table   | pt  |   Print table representation                            |
+|  Find Edge     | f   |   Check if the edge exists in the graph                 |
+|  BFS           | bfs |   Breath-first search                                   |
+|  DFS           | dfs |   Depth-first search                                    |
+|  Kahn          | ks  |   Topological sort with Kahn algorithm                  |
+|  Tarjan        | ts  |   Topological sort with Tarjan algorithm                |
+|  Exit          | q   |   Exits the program (same as ctrl+D)                    |
++================================================================================+''')
         case 'exit' | 'q':
             break
         case _:
